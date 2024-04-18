46203a314eba822603f4cbb9c76c51d438aa8b2b
Castro456 <castrosid456@gmail.com>
2024-04-18 17:00:27 +0530

    login, logout auth validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -31,4 +31,38 @@ class AuthController extends Controller
 
         return redirect()->route('dashboard')->with('success', 'Account created successfully');
     }
+
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function authenticate()
+    {
+        $validate = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(auth()->attempt($validate)) {
+            request()->session()->regenerate();
+
+            return redirect()->route('dashboard')->with('success', 'Logged in successfully');
+        }
+        else {
+            return redirect()->route('login')->withErrors([
+                'email' => 'Email or password is incorrect'
+            ]);
+        }
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+
+        return redirect()->route('dashboard')->with('success', 'Logged out successfully');
+    }
 }
