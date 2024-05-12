60909d25a89b6ce91d88849689b1c10fd58b2c5c
Castro456 <castrosid456@gmail.com>
2024-05-12 20:56:48 +0530

    added eager loading

--- app/Models/TwitterCloneModel.php
+++ app/Models/TwitterCloneModel.php
@@ -8,12 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class TwitterCloneModel extends Model
 {
     use HasFactory;
+
+    /**
+     * It is eager loading and it reduces the db hit, in a single it will get the relationship data's.
+     * 
+     * first parameter for users, for each and every time we need to show user name in the tweet. and specified only the columns that we need. 
+     * 
+     * second parameter for comments
+     * 
+     * Need to study how eager loading works in detail.
+     * 
+     *  */  
+    protected $with = ['user:id,name,image', 'twitter_comments'];
+
     protected $table = "twitter_clone"; //define this line or else it will throw an error like no database found 'twitter_clone"
 
     protected $fillable = [
         'user_id',
-        'content',
-        'likes'
+        'content'
     ]; //What columns are assignable
 
     public function twitter_comments() //name of the function should be the name of the table that has the relationship with [Wrong statement, fn name can be anything].
