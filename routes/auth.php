32aaa9b23019de8461ef23254e997a7abcd8abb2
Castro456 <castrosid456@gmail.com>
2024-05-23 23:38:17 +0530

    added guest middleware

--- routes/auth.php
+++ routes/auth.php
@@ -8,12 +8,17 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/register', [AuthController::class, 'register'])->name('register');
+/**
+ * guest middleware is opposite of auth, it will allow user only if they are not logged in
+ */
+Route::group(['middleware' => 'guest'], function() {
+  Route::get('/register', [AuthController::class, 'register'])->name('register');
+  
+  Route::post('/register', [AuthController::class, 'store']);
+  
+  Route::get('/login', [AuthController::class, 'login'])->name('login');
+  
+  Route::post('/login', [AuthController::class, 'authenticate']);
+});
 
-Route::post('/register', [AuthController::class, 'store']);
-
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-
-Route::post('/login', [AuthController::class, 'authenticate']);
-
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
